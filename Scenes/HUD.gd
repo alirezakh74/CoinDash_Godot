1c33ff3bfe363e6edc74e434c1adb0976a1939e6
alirezakh74 <alirezakhodabande74@gmail.com>
2023-11-24 00:02:23 +0330

    upload

---
+++ Scenes/HUD.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+signal new_game
+signal show_game_over_end
+
+func update_score(value):
+	$MarginContainer/ScoreLabel.text = str(value)
+
+func update_timer(value):
+	$MarginContainer/TimeLabel.text = str(value)
+
+func show_message(text):
+	$MessageLabel.text = text
+	$MessageLabel.show()
+	$MessageTimer.start()
+
+
+func _on_MessageTimer_timeout():
+	$MessageLabel.hide()
+
+
+func _on_StartButton_pressed():
+	$StartButton.hide()
+	$MessageLabel.hide()
+	emit_signal("new_game")
+
+func show_game_over():
+	show_message("Game Over")
+	yield($MessageTimer, "timeout")
+	emit_signal("show_game_over_end")
+	$StartButton.show()
+	$MessageLabel.text = "Coin Dash!"
+	$MessageLabel.show()
