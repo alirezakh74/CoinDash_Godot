bd906a8bebc726be01c497a3d132f77ae0482b79
alirezakh74 <alirezakhodabande74@gmail.com>
2023-11-24 16:54:14 +0330

    add persian stuff

--- Scenes/HUD.gd
+++ Scenes/HUD.gd
@@ -4,9 +4,12 @@ signal new_game
 signal show_game_over_end
 
 func update_score(value):
+	value = cast_to_persian_number(value)
 	$MarginContainer/ScoreLabel.text = str(value)
 
 func update_timer(value):
+	#print("time is: " + str(value))
+	value = cast_to_persian_number(value)
 	$MarginContainer/TimeLabel.text = str(value)
 
 func show_message(text):
@@ -31,3 +34,33 @@ func show_game_over():
 	$StartButton.show()
 	$MessageLabel.text = "Coin Dash!"
 	$MessageLabel.show()
+
+func cast_to_persian_number(value):
+	var finalStr : String = ""
+	var myStr : String = str(value)
+	var myChars : Array = []
+	for c in myStr:
+		myChars.push_back(c)
+	for mychar in myChars:
+		if mychar == "0":
+			finalStr += "٠"
+		if mychar == "1":
+			finalStr += "١"
+		if mychar == "2":
+			finalStr += "٢"
+		if mychar == "3":
+			finalStr += "٣"
+		if mychar == "4":
+			finalStr += "٤"
+		if mychar == "5":
+			finalStr += "٥"
+		if mychar == "6":
+			finalStr += "٦"
+		if mychar == "7":
+			finalStr += "٧"
+		if mychar == "8":
+			finalStr += "٨"
+		if mychar == "9":
+			finalStr += "٩"
+	#print(finalStr)
+	return finalStr
